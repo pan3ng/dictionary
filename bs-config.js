b08dad9e8a65be8f26f94d8d52a986479181587d
pan3ng <mphopan@gmail.com>
2020-02-23 21:19:01 +0200

    bs configuration file

---
+++ bs-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    browser: "C:/Program Files/Firefox Developer Edition/firefox.exe",
+
+    //  files: ["*.css", "*.py", "*.html"],
+    files: "*",
+
+    exclude: false,
+
+    server: false,
+
+    injectChanges: true,
+
+    proxy: "localhost:5000",
+
+    port: 5000
+};
